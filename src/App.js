d9fefc2f640b8ed9a269e518340cdee1c75e5527
Toshi <seuzawa.toshihiro@aspasio.com>
2024-10-30 09:58:53 +0900

    Next.js超入門 　Section 2-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const msg = "これはサンプルのメッセージです"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={props.color}>{props.title}</h1>
+      <p className={props.color}>{props.message}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App color="red" title="Hello!" message="これえは属性の値です" />
   </React.StrictMode>
 );
 
